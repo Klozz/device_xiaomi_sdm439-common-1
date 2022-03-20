8c6454d9b93a84ce80d73846d673883f4a36c987
Jake <yapjakebrian1@gmail.com>
2022-03-21 06:43:17 +0800

    [TEST]Revert "Remove packages at build time"
    This reverts commit 9ddf8aae417a0da760b57c3ec181347b2d62fb97.

--- sdm439.mk
+++ sdm439.mk
@@ -355,10 +355,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    Remove_Packages
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
